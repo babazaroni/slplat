e950f716869faaf87e5299f5a8245f5a3d433201
babazaroni <babazaroni@gmail.com>
2021-07-31 22:26:19 -0700

    cleaning

--- slplat.py
+++ slplat.py
@@ -7,35 +7,28 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from slplat_utils import *
 
-#import your plat descriptors from your definition file here
+#import plat descriptors from definition file here
 from myproperty import pix
 
-
 st.set_page_config(layout='wide')
-
 mpl.rcParams['lines.linewidth'] = 1
 
-
-
-print('starting')
-
-fig3, ax3 = plt.subplots()
-ax3.axis('equal')
-
+fig, ax = plt.subplots()
+ax.axis('equal')
 
 for pi in pix:
     if st.sidebar.checkbox(pi.text,False):
         pi.selected = True
         segments = get_path_points(pi.origin,pi.path,1,-1) # change y coordinate to match matplotlib
         for segment in segments:
-            ax3.plot(segment[0],segment[1])
+            ax.plot(segment[0],segment[1])
 
         if pi.aux_info:
-            ax3.text(pi.aux_info['pos_x'],pi.aux_info['pos_y'],pi.aux_info['text'])
+            ax.text(pi.aux_info['pos_x'],pi.aux_info['pos_y'],pi.aux_info['text'])
     else:
         pi.selected = False
 
-st.pyplot(fig3)
+st.pyplot(fig)
 
 for pi in pix:
     if pi.selected:
